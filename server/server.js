3e9bf54b743bbd424d669bb04c8cebd4788e2528
Anish Yadav <hney1000.ay1@gmail.com>
2018-12-05 20:15:05 +0530

    Added the authenticate middleware and private routes too

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+require('./config/config');
+
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -5,7 +8,8 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
-var {User} = require('./models/user.js')
+var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -14,9 +18,10 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 //POST Todos
-app.post('/todos',(req,res) =>{
+app.post('/todos',authenticate,(req,res) =>{
         var todo = new Todo ({
-          text : req.body.text
+          text : req.body.text,
+          _creator : req.user._id
         });
 
         todo.save().then((docs) =>{
@@ -27,8 +32,10 @@ app.post('/todos',(req,res) =>{
 });
 
 //GET all todos
-app.get('/todos',(req,res)=>{
-      Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+      Todo.find({
+        _creator : req.user._id
+      }).then((todos)=>{
         res.send({todos});
       },(e)=>res.status(400).send(e));
 });
@@ -36,7 +43,7 @@ app.get('/todos',(req,res)=>{
 
 
 //GET todo by id
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
    
       var id = req.params.id;
 
@@ -44,25 +51,31 @@ app.get('/todos/:id',(req,res)=>{
          return res.status(404).send();
       }
        
-      Todo.findById(id).then((todo)=>{
+      Todo.findOne({
+       _id : id,
+       _creator : req.user._id
+      }).then((todo)=>{
         if(!todo){
           return res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
       });
 
 
 });
 
 //DELETE todos 
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
  var id = req.params.id;
 
  if(!ObjectID.isValid(id)){
   return res.status(404).send();
  }
 
- Todo.findByIdAndRemove(id).then((todo)=>{
+ Todo.findOneAndRemove({
+  _id : id,
+  _creator : req.user._id
+ }).then((todo)=>{
  if(!todo){
   return res.status(404).send();
  }
@@ -76,7 +89,7 @@ app.delete('/todos/:id',(req,res)=>{
 
 
 // PATCH Todos
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
       var id = req.params.id;
       var body = _.pick(req.body,['text','completed']);
 
@@ -91,7 +104,10 @@ app.patch('/todos/:id',(req,res)=>{
        body.completedAt = null;
       }
 
-      Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+      Todo.findOneAndUpdate({
+        _id : id,
+        _creator : req.user._id
+      },{$set:body},{new:true}).then((todo)=>{
         if(!todo){
           return res.status(404).send()
         }
@@ -104,6 +120,53 @@ app.patch('/todos/:id',(req,res)=>{
 });
 
 
+/////////////USERS //////////////////////////////
+
+app.post('/users',(req,res)=>{
+      
+      var body = _.pick(req.body,['email','password']);
+      var user = new User(body);
+                                
+      user.save().then(()=>{
+         return user.generateAuthToken();
+      }).then((token)=>{
+        res.header('x-auth',token).send(user);
+      }).catch((e)=>{
+        res.status(400).send(e);
+      });
+
+
+
+});
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+
+});
+
+
+//POST User login 
+app.post('/users/login',(req,res)=>{
+  var body = _.pick(req.body,['email','password']);
+
+  User.findByCredential(body.email,body.password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+    res.header('x-auth',token).send(user);
+     });
+   }).catch((e)=>{
+    res.status(400).send();
+  })
+});
+
+//DELETE token
+app.delete('/users/me/token',authenticate,(req,res)=>{
+   
+req.user.deleteToken(req.token).then(()=>{
+  res.status(200).send()
+},()=>{
+  res.status(400).send();
+})
+});
 
 
 
